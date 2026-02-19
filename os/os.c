8ae7b1d3a714b38a1f308d13b7ad2c2d5e83becf
Shreem Srivastava <shreemsrivastava@gmail.com>
2026-02-19 15:52:05 +0530

    update os.c

--- os/os.c
+++ os/os.c
@@ -1,50 +1,69 @@
 #include <stdio.h>
 
 int main() {
-    int n, i;
-    int burst_time[20], waiting_time[20], turnaround_time[20];
-    float avg_waiting_time = 0.0, avg_turnaround_time = 0.0;
+    int n, i, j;
+    int at[20], bt[20], wt[20], tat[20], ct[20], rt[20], pid[20], temp;
+    float avg_wt = 0, avg_tat = 0;
 
-    printf("Enter total number of processes (max 20): ");
+    printf("Enter number of processes: ");
     scanf("%d", &n);
 
-    if (n <= 0 || n > 20) {
-        printf("Invalid number of processes.\n");
-        return 1;
-    }
-
-    printf("\nEnter process burst time:\n");
     for (i = 0; i < n; i++) {
-        printf("P[%d]: ", i + 1);
-        scanf("%d", &burst_time[i]);
+        pid[i] = i + 1;
+        printf("\nProcess P[%d]\n", i + 1);
+        printf("Arrival Time: ");
+        scanf("%d", &at[i]);
+        printf("Burst Time: ");
+        scanf("%d", &bt[i]);
     }
 
-    // First process waiting time = 0
-    waiting_time[0] = 0;
+    for (i = 0; i < n - 1; i++) {
+        for (j = i + 1; j < n; j++) {
+            if (at[j] < at[i]) {
+                temp = at[i];
+                at[i] = at[j];
+                at[j] = temp;
+
+                temp = bt[i];
+                bt[i] = bt[j];
+                bt[j] = temp;
+
+                temp = pid[i];
+                pid[i] = pid[j];
+                pid[j] = temp;
+            }
+        }
+    }
+
+    ct[0] = at[0] + bt[0];
 
     for (i = 1; i < n; i++) {
-        waiting_time[i] = burst_time[i - 1] + waiting_time[i - 1];
+        if (ct[i - 1] < at[i])
+            ct[i] = at[i] + bt[i];
+        else
+            ct[i] = ct[i - 1] + bt[i];
     }
-
-    printf("\nProcess\tBurst Time\tWaiting Time\tTurnaround Time\n");
 
     for (i = 0; i < n; i++) {
-        turnaround_time[i] = burst_time[i] + waiting_time[i];
-        avg_waiting_time += waiting_time[i];
-        avg_turnaround_time += turnaround_time[i];
+        tat[i] = ct[i] - at[i];
+        wt[i] = tat[i] - bt[i];
+        rt[i] = wt[i];
 
-        printf("P[%d]\t%d\t\t%d\t\t%d\n",
-               i + 1,
-               burst_time[i],
-               waiting_time[i],
-               turnaround_time[i]);
+        avg_wt += wt[i];
+        avg_tat += tat[i];
     }
 
-    avg_waiting_time /= n;
-    avg_turnaround_time /= n;
+    avg_wt /= n;
+    avg_tat /= n;
 
-    printf("\nAverage Waiting Time = %.2f\n", avg_waiting_time);
-    printf("Average Turnaround Time = %.2f\n", avg_turnaround_time);
+    printf("\nPID\tAT\tBT\tCT\tTAT\tWT\tRT\n");
+    for (i = 0; i < n; i++) {
+        printf("P[%d]\t%d\t%d\t%d\t%d\t%d\t%d\n",
+               pid[i], at[i], bt[i], ct[i], tat[i], wt[i], rt[i]);
+    }
+
+    printf("\nAverage Waiting Time = %.2f", avg_wt);
+    printf("\nAverage Turnaround Time = %.2f\n", avg_tat);
 
     return 0;
 }
